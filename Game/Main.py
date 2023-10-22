1ec0d4af0f092a4f5c017e0c29637eed072a69f1
ChamChiCham@Laptop <kyjp0623@gmail.com>
2023-10-22 13:43:38 +0900

    정확한 물리 구현을 위한 프레임 고정 (FPS: 100)

--- Game/Main.py
+++ Game/Main.py
@@ -54,6 +54,9 @@ while running:
     handle_events()
     update_world()
     render_world()
-    delay(0.01)
+    time_elapsed = get_time() - time_started
+    if time_elapsed > 0.01:
+        time_elapsed = 0.01
+    delay(0.01 - time_elapsed)
 # finalization code
 close_canvas()
