4cf0cdf87d4ad166fea516ab5847a85c32bcac1f
ChamChiCham@Laptop <kyjp0623@gmail.com>
2023-10-22 14:28:32 +0900

    배경 중 당구대 추가

--- Game/background.py
+++ Game/background.py
@@ -1,14 +1,24 @@
+# #####
+# background.py: 배경 클래스가 구현되어 있는 파일.
+# #####
 from pico2d import load_image
 
 class Background:
-    image = None
+
+    # 이미지 변수
+    image_background = None
+    image_board = None
 
     def __init__(self):
-        if self.image == None:
-            self.image = load_image('image_background_playing.jpg')
+        if self.image_background == None:
+            self.image_background = load_image('image_background_playing.jpg')
+
+        if self.image_board == None:
+            self.image_board = load_image('image_board.png')
 
     def draw(self):
-        self.image.draw(1280 // 2, 960 // 2)
+        self.image_background.draw(1280 // 2, 960 // 2)
+        self.image_board.draw(1280 // 2, 960 // 2)
 
     def update(self):
         pass
